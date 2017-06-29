d8328deaf45f5e97315df7d1315e3a4421120ef6
Lenovo <Lenovo@Y50>
2017-06-29 18:05:17 +0300

    Add project files.

---
+++ ArrayExtension/ArrayExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExtension
+{
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// A class which provides a method for sorting an integer array using QuickSort
+        /// </summary>
+        public static void Sort(int[] array)
+        {
+            /// <summary>
+            /// Sorts an array of integer
+            /// </summary>
+            /// <param name="array">
+            /// Integer array to sort
+            /// </param>
+            CheckArray(array);
+            SortPart(array, 0, array.Length - 1);
+        }
+        private static void SortPart(int[] array, int l, int r)
+        {
+            int x = array[l + (r - l) / 2];
+            int i = l;
+            int j = r;
+
+            int temp;
+            while (i <= j)
+            {
+                while (array[i] < x)
+                    i++;
+                while (array[j] > x)
+                    j--;
+
+                if (i <= j)
+                {
+                    temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                    i++;
+                    j--;
+                }
+
+                if (i < r)
+                    SortPart(array, i, r);
+                if (l < j)
+                    SortPart(array, l, j);
+            }
+        }
+        private static void CheckArray(int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                throw new ArgumentException("Array is empty");
+        }
+    }
+}
---
+++ ConsoleUI/Demo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            QSortDemo();
+            return;
+        }
+        public static void QSortDemo()
+        {
+            Random r = new Random();
+            int n = 20;
+            int[] arrayToSort = new int[n];
+            Console.WriteLine("Qsort demonstration.");
+            Console.WriteLine("Array:");
+            for (int i = 0; i < n; i++)
+            {
+                arrayToSort[i] = r.Next() % 1000;
+                Console.Write("{0} ", arrayToSort[i]);
+            }
+            Console.WriteLine();
+            ArrayExtension.ArrayExtension.Sort(arrayToSort);
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", arrayToSort[i]);
+            }
+            Console.WriteLine();
+            Console.Write("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
